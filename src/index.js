c7fa2745db608e10f0348ed56d04aca9ae19d688
zhoushoujian <qianchengyimeng@qq.com>
2018-08-08 21:18:29 +0800

    pickApples-redux

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 const store = createStore(appStore);
 {/* Provider是一个普通组件，可以作为顶层app的分发点，它只需要store属性就可以了。它会将state分发给所有被connect的组件，不管它在哪里，被嵌套多少层。 */}
-ReactDOM.render(
-<Provider store={store}>   
-    <App />   
-</Provider>
-, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(
+// <Provider store={store}>   
+//     <App />   
+// </Provider>
+// , document.getElementById('root'));
+// registerServiceWorker();
