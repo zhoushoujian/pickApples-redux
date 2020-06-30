1a697fddc08c41ee08a129befeea21f9991651d7
shoujian.zhou <shoujian.zhou@shuyun.com>
2020-06-30 16:23:38 +0800

    redux-demo

---
+++ src/apple.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import CurrentApple from "./component/currentApple";
+import EatenApple from "./component/eatenApple";
+import AppleList from "./component/appleList";
+import "./apple.css";
+
+const Apple = () => {
+  return (
+    <div className="appleBasket">
+      <div className="title">苹果篮子(redux)</div>
+      <div className="stats">
+        <CurrentApple />
+        <EatenApple />
+      </div>
+      <AppleList />
+    </div>
+  );
+};
+
+export default Apple;
--- src/ducks/index.js
+++ src/ducks/index.js
@@ -1,61 +1,63 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from "redux";
 
 //actionType
-const PICK_APPLE = "PICK_APPLE"
-const CHANGE_WEIGHT = "CHANGE_WEIGHT"
-const EAT_APPLE = "EAT_APPLE"
+const PICK_APPLE = "PICK_APPLE";
+const CHANGE_WEIGHT = "CHANGE_WEIGHT";
+const EAT_APPLE = "EAT_APPLE";
 
 const initialState = [
-	{
-		name: parseInt(Math.random() * 100000, 10),
-		weight: parseInt(Math.random() * 1000, 10),
-		id: 0,
-		eaten: false
-	}
-]
+  {
+    name: parseInt(Math.random() * 100000),
+    weight: parseInt(Math.random() * 1000),
+    id: 0,
+    eaten: false,
+  },
+];
 
 //reducer
 const apples = (state = initialState, action) => {
-    switch(action.type) {
-        case PICK_APPLE:
-            return [
-                ...state,
-                {
-                    name: parseInt(Math.random() * 100000, 10),
-                    weight: parseInt(Math.random() * 1000, 10),
-                    id: action.id,
-                    eaten: false
-                }
-            ]
-        case CHANGE_WEIGHT:
-            return state.map((v) => v.id === action.apple.id ? { ...v, weight: parseInt(Math.random() * 1000, 10)} : v)
-        case EAT_APPLE :
-            return state.map((v) => v.id === action.apple.id ? {...v, eaten: true} : v)
-        default: 
-            return state
-    }
-}
-
+  switch (action.type) {
+    case PICK_APPLE:
+      return [
+        ...state,
+        {
+          name: parseInt(Math.random() * 100000),
+          weight: parseInt(Math.random() * 1000),
+          id: action.id,
+          eaten: false,
+        },
+      ];
+    case CHANGE_WEIGHT:
+      return state.map((v) => (v.id === action.apple.id
+        ? { ...v, weight: parseInt(Math.random() * 1000) }
+        : v));
+    case EAT_APPLE:
+      return state.map((v) => (v.id === action.apple.id ? { ...v, eaten: true } : v));
+    default:
+      return state;
+  }
+};
 
 export const appStore = combineReducers({
-	apples
-})
+  apples,
+});
 
-let index = 1
+let index = 1;
 //actions
 export const pickApple = (apple) => {
-    return {
-        type: PICK_APPLE,   //一般 type 的内容使用 大写字母+下划线 的格式。
-        id: index++,
-        apple
-    }  
-}
+  return {
+    type: PICK_APPLE, //一般 type 的内容使用 大写字母+下划线 的格式。
+    // eslint-disable-next-line no-plusplus
+    id: index++,
+    apple,
+  };
+};
 
 export const changeWeight = (apple) => ({
-	type: CHANGE_WEIGHT,
-	apple
+  type: CHANGE_WEIGHT,
+  apple,
 });
 export const eatApple = (apple) => ({
-	type: EAT_APPLE,
-	apple
+  type: EAT_APPLE,
+  apple,
 });
