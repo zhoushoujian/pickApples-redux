bbc897d72dcb78bf70c117b980ebaea94e88708a
shoujian.zhou <shoujian.zhou@shuyun.com>
2020-07-30 16:49:13 +0800

    修复eslint检查

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,12 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import Apple from './apple';
 import registerServiceWorker from './registerServiceWorker';
-import { appStore } from './ducks/index'
+import { appStore } from './ducks/index';
 import './index.css';
 
 const store = createStore(appStore);
-window.$getState = store.getState
-console.log("$getState()['apples']", window.$getState().apples)
+window.$getState = store.getState;
+console.log("$getState()['apples']", window.$getState().apples);
 ReactDOM.render(
     <Provider store={store}>   
         <Apple />   
