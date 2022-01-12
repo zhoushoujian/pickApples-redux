82e79f8f5fdfb214fa8a5a1ec4f9483497f00017
zhoushoujian <shoujian.zhou@shuyun.com>
2022-01-12 10:29:58 +0800

    less

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import Apple from './apple';
-import registerServiceWorker from './registerServiceWorker';
-import { appStore } from './ducks/index';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import ChangeSkin from "./others/change-skin";
+import registerServiceWorker from "./registerServiceWorker";
+import { appStore } from "./ducks/index";
+import "./index.css";
 
 export const store = createStore(appStore);
 console.log("store.getState().apples)", store.getState().apples);
 ReactDOM.render(
-    <Provider store={store}>   
-        <Apple />   
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <ChangeSkin />
+  </Provider>,
+  document.getElementById("root")
 );
 registerServiceWorker();
--- src/others/change-skin/index.jsx
+++ src/others/change-skin/index.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import './index.css';
+import React from "react";
+import "./index.less";
 
-const colors = ['#000', '#ff0', '#f00', '#f0f', '#0ff', '#00f', '#0f0'];
+const colors = ["#000", "#ff0", "#f00", "#f0f", "#0ff", "#00f", "#0f0"];
 
 //这里只是用css来举例，实际上css module, less和sass也是可以的，原理是利用css变量来替代less变量或sass变量，然后使用更改css变量的方式来改变css
 const ChangeSkin = ({ text = "click me" }) => {
-
   const changeColor = () => {
     const root = document.documentElement;
-    root.style.setProperty("--primary-color", colors[Math.floor(Math.random() * colors.length)]);
+    root.style.setProperty(
+      "--primary-color",
+      colors[Math.floor(Math.random() * colors.length)]
+    );
   };
 
   return (
-    <div className="loading" onClick={changeColor}>{text}</div>
+    <div className="loading" onClick={changeColor}>
+      {text}
+    </div>
   );
 };
 
